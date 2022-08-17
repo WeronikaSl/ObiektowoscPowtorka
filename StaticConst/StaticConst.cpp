214717c31cbc4042c872adbf3039f4c358700271
Weronika Slowinska <weronikaslowinska33@gmail.com>
2022-08-17 10:47:06 +0200

    StaticConst

---
+++ StaticConst/StaticConst.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+
+class Cat
+{
+private:
+    const int _paws = 4;
+    std::string _name = {};
+
+public:
+
+    Cat(std::string name) : _name(name)
+    {}
+    Cat(const Cat& old)
+    {
+        this->_name = old._name;
+    }
+    std::string getName()
+    {
+        return _name;
+    }
+
+};
+
+
+
+int main()
+{
+    Cat c1("Sernik");
+    Cat c2 = c1;
+    std::cout << c2.getName() << std::endl;
+
+}
